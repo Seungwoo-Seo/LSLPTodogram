75db472f269033100fb831578ad169d9610c5505
SeungwooSeo <prodvssw.98@gmail.com>
2023-11-20 20:28:39 +0900

    [Add] Color 파일 추가
    systemBackground
    systemBlue
    black
    white
    red
    green
    clear
    gray
    lightGray
    darkGray

---
+++ LSLPTodogram/Constraints/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  LSLPTodogram
+//
+//  Created by 서승우 on 2023/11/20.
+//
+
+import UIKit
+
+enum Color {
+    static let systemBackground = UIColor.systemBackground
+    static let systemBlue = UIColor.systemBlue
+    static let black = UIColor.black
+    static let white = UIColor.white
+    static let red = UIColor.red
+    static let green = UIColor.green
+    static let clear = UIColor.clear
+    static let gray = UIColor.gray
+    static let lightGray = UIColor.lightGray
+    static let darkGray = UIColor.darkGray
+}
