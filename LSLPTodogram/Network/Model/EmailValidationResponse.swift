742ff65d4c59e001abc106e1a0104ab8582925b2
SeungwooSeo <prodvssw.98@gmail.com>
2023-11-23 16:02:40 +0900

    [Feat] EmailValidationResponse 모델 추가

---
+++ LSLPTodogram/Network/Model/EmailValidationResponse.swift
@@ -0,0 +1,12 @@
+//
+//  EmailValidationResponse.swift
+//  LSLPTodogram
+//
+//  Created by 서승우 on 2023/11/23.
+//
+
+import Foundation
+
+struct EmailValidationResponse: Decodable {
+    let message: String
+}
