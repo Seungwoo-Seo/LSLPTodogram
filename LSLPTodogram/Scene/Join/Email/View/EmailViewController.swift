223cd46f3bc605596109eaa0bc30d356b9d23b17
SeungwooSeo <prodvssw.98@gmail.com>
2023-11-23 16:37:09 +0900

    [Feat] 이메일 검증 네트워킹 스트림 구현
    성공 했을 때
    - alert present 처리
    
    실패 했을 때
    - localError: alert present 처리
    - emailValidationError: alert present 처리

--- LSLPTodogram/Scene/Join/Email/View/EmailViewController.swift
+++ LSLPTodogram/Scene/Join/Email/View/EmailViewController.swift
@@ -6,9 +6,14 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 final class EmailViewController: BaseViewController {
     private let mainView = EmailMainView()
+
+    private let disposeBag = DisposeBag()
+    private let viewModel = EmailViewModel()
 
     override func loadView() {
         view = mainView
@@ -16,7 +21,42 @@ final class EmailViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        bind()
+    }
+
+    private func bind() {
+        let input = EmailViewModel.Input(
+            emailText: mainView.emailTextField.rx.text,
+            emailValidationButtonTapped: mainView.emailValidationButton.rx.tap,
+            nextButtonTapped: mainView.nextButton.rx.tap
+        )
+
+        let output = viewModel.transform(input: input)
+        output.localError
+            .bind(with: self) { owner, localError in
+                owner.presentAlert(title: localError)
+            }
+            .disposed(by: disposeBag)
+
+        output.emailValidationError
+            .bind(with: self) { owner, emailValidationError in
+                owner.presentAlert(title: emailValidationError.description)
+            }
+            .disposed(by: disposeBag)
+
+        output.emailSuccessMessage
+            .bind(with: self) { owner, message in
+                owner.presentAlert(title: message)
+            }
+            .disposed(by: disposeBag)
+    }
+
+    private func presentAlert(title: String) {
+        let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        let confirm = UIAlertAction(title: "확인", style: .default)
+        alert.addAction(confirm)
+        present(alert, animated: true)
     }
 
 }
--- LSLPTodogram/Scene/Join/Email/ViewModel/EmailViewModel.swift
+++ LSLPTodogram/Scene/Join/Email/ViewModel/EmailViewModel.swift
@@ -6,19 +6,65 @@
 //
 
 import Foundation
+import RxCocoa
+import RxSwift
 
 final class EmailViewModel: ViewModelType {
+    private let disposeBag = DisposeBag()
 
     struct Input {
-
+        let emailText: ControlProperty<String?>
+        let emailValidationButtonTapped: ControlEvent<Void>
+        let nextButtonTapped: ControlEvent<Void>
     }
 
     struct Output {
-
+        let localError: PublishRelay<String>
+        let emailValidationError: PublishRelay<EmailValidationError>
+        let emailSuccessMessage: PublishRelay<String>
     }
 
     func transform(input: Input) -> Output {
-        return Output()
+        let localError = PublishRelay<String>()
+        let emailValidationError = PublishRelay<EmailValidationError>()
+        let emailSuccessMessage = PublishRelay<String>()
+
+        let email = input.emailValidationButtonTapped
+            .withLatestFrom(input.emailText.orEmpty)
+            .asDriver(onErrorJustReturn: "")
+
+        email
+            .filter { $0.isEmpty }
+            .drive(with: self) { owner, _ in
+                localError.accept("이메일을 입력하세요.")
+            }
+            .disposed(by: disposeBag)
+
+        email
+            .filter { !$0.isEmpty }
+            .asObservable()
+            .flatMapLatest {
+                return NetworkManager.shared.request(
+                    type: EmailValidationResponse.self,
+                    api: Router.emailChek(email: $0)
+                )
+                .catch { error in
+                    let statusCode = error.asAFError?.responseCode ?? 500
+                    let evError = EmailValidationError(rawValue: statusCode) ?? .서버_에러
+                    emailValidationError.accept(evError)
+                    return Single.never()
+                }
+            }
+            .bind(with: self) { owner, response in
+                emailSuccessMessage.accept(response.message)
+            }
+            .disposed(by: disposeBag)
+
+        return Output(
+            localError: localError,
+            emailValidationError: emailValidationError,
+            emailSuccessMessage: emailSuccessMessage
+        )
     }
 
 }
