a695dc27abb0a264bf7d9f4e17163688d943cc1c
SeungwooSeo <prodvssw.98@gmail.com>
2023-11-24 23:52:56 +0900

    [Feat] 가입 버튼 눌렀을 때 닉네임 필수 입력 처리

--- LSLPTodogram/Scene/Join/UserDetail/View/UserDetailViewController.swift
+++ LSLPTodogram/Scene/Join/UserDetail/View/UserDetailViewController.swift
@@ -6,9 +6,14 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 final class UserDetailViewController: BaseViewController {
     private let mainView = UserDetailMainView()
+
+    private let disposeBag = DisposeBag()
+    private let viewModel = UserDetailViewModel()
 
     override func loadView() {
         view = mainView
@@ -16,7 +21,26 @@ final class UserDetailViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        bind()
+    }
+
+    private func bind() {
+        let input = UserDetailViewModel.Input(
+            nicknameText: mainView.nicknameView.textField.rx.text,
+            phoneNubText: mainView.phoneNumView.textField.rx.text,
+            birthDayText: mainView.birthDayView.textField.rx.text,
+            joinButtonTapped: mainView.completeButton.rx.tap
+        )
+
+        let output = viewModel.transform(input: input)
+        output.nicknameIsEmpty
+            .bind(with: self) { owner, bool in
+                owner.mainView.nicknameView.errorLabel.isHidden = false
+                owner.mainView.nicknameView.errorLabel.text = bool ? "닉네임 입력은 필수입니다." : ""
+                owner.mainView.nicknameView.errorLabel.textColor = bool ? Color.red : Color.green
+            }
+            .disposed(by: disposeBag)
     }
 
 }
--- LSLPTodogram/Scene/Join/UserDetail/ViewModel/UserDetailViewModel.swift
+++ LSLPTodogram/Scene/Join/UserDetail/ViewModel/UserDetailViewModel.swift
@@ -6,19 +6,62 @@
 //
 
 import Foundation
+import RxCocoa
+import RxSwift
 
 final class UserDetailViewModel: ViewModelType {
+    private let disposeBag = DisposeBag()
 
     struct Input {
-
+        let nicknameText: ControlProperty<String?>
+        let phoneNubText: ControlProperty<String?>
+        let birthDayText: ControlProperty<String?>
+        let joinButtonTapped: ControlEvent<Void>
     }
 
     struct Output {
-
+        let nicknameIsEmpty: PublishRelay<Bool>
     }
 
     func transform(input: Input) -> Output {
-        return Output()
+        let nicknameIsEmpty = PublishRelay<Bool>()
+
+        let combineTexts = Observable
+            .combineLatest(
+                input.nicknameText.orEmpty,
+                input.phoneNubText.orEmpty,
+                input.birthDayText.orEmpty
+            )
+
+        let userDetailTexts = input.joinButtonTapped
+            .withLatestFrom(combineTexts)
+            .share()
+
+        userDetailTexts
+            .filter { $0.0.isEmpty }
+            .debug()
+            .bind(with: self) { owner, _ in
+                nicknameIsEmpty.accept(true)
+            }
+            .disposed(by: disposeBag)
+
+        userDetailTexts
+            .filter { !$0.0.isEmpty }
+            // TODO: 정규식 추가
+            .flatMapLatest { texts in
+                return NetworkManager.shared.request(
+                    type: JoinDTO.self,
+                    api: Router.join(email: texts.0, password: texts.1, nick: texts.2, phoneNum: nil, birthDay: nil)
+                )
+            }
+            .bind(with: self) { owner, response in
+                // NOTE: 여길 할게 없긴할거 같긴해
+            }
+            .disposed(by: disposeBag)
+
+        return Output(
+            nicknameIsEmpty: nicknameIsEmpty
+        )
     }
 
 }
