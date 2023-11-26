f675f4022baac6693f2d7a137234145a8ed8d494
SeungwooSeo <prodvssw.98@gmail.com>
2023-11-26 23:52:34 +0900

    [Refactor] PasswordMainView InfoInput 컴포넌트로 리팩토링

--- LSLPTodogram/Scene/Join/Password/View/PasswordMainView.swift
+++ LSLPTodogram/Scene/Join/Password/View/PasswordMainView.swift
@@ -8,78 +8,22 @@
 import UIKit
 
 final class PasswordMainView: BaseView {
-    let titleLabel = {
-        let view = UILabel()
-        view.textColor = Color.black
-        view.font = .systemFont(ofSize: 24, weight: .bold)
-        view.text = "비밀번호 입력"
-        return view
-    }()
-    let passwordLabel = {
-        let view = UILabel()
-        view.textColor = Color.black
-        view.font = .systemFont(ofSize: 17, weight: .regular)
-        view.text = "비밀번호를 입력해주세요."
-        return view
-    }()
-    let passwordTextField = {
-        let view = UITextField()
-        view.textColor = Color.black
-        view.borderStyle = .roundedRect
-        return view
-    }()
-    let sameStackView = {
-        let view = UIStackView()
-        view.axis = .vertical
-        view.alignment = .fill
-        view.distribution = .fillProportionally
-        view.spacing = 8
-        return view
-    }()
-    let sameLabel = {
-        let view = UILabel()
-        view.textColor = Color.black
-        view.font = .systemFont(ofSize: 17, weight: .regular)
-        view.text = "비밀번호를 한 번 더 입력해주세요."
-        return view
-    }()
-    let sameTextField = {
-        let view = UITextField()
-        view.textColor = Color.black
-        view.borderStyle = .roundedRect
-        return view
-    }()
-    let sameErrorLabel = {
-        let view = UILabel()
-        view.textColor = Color.red
-        view.font = .systemFont(ofSize: 17, weight: .regular)
-        view.isHidden = true
-        return view
-    }()
-    let nextButton = {
-        var config = UIButton.Configuration.filled()
-        config.cornerStyle = .capsule
-        config.title = "다음"
-        let button = UIButton(configuration: config)
-        return button
-    }()
+    private let titleLabel = InfoInputTitleLabel(text: "비밀번호 입력")
+    let passwordView = InfoInputView(description: "비밀번호를 입력해주세요.")
+    let reconfirmView = InfoInputView(description: "비밀번호를 한 번 더 입력해주세요.")
+    let nextButton = InfoInputButton(title: "다음")
+    let prevButton = InfoInputButton(title: "이전")
 
     override func initialHierarchy() {
         super.initialHierarchy()
 
         [
             titleLabel,
-            passwordLabel,
-            passwordTextField,
-            sameStackView,
-            nextButton
+            passwordView,
+            reconfirmView,
+            nextButton,
+            prevButton
         ].forEach { addSubview($0) }
-
-        [
-            sameLabel,
-            sameTextField,
-            sameErrorLabel
-        ].forEach { sameStackView.addArrangedSubview($0) }
     }
 
     override func initialLayout() {
@@ -93,24 +37,24 @@ final class PasswordMainView: BaseView {
             make.horizontalEdges.equalToSuperview().inset(inset)
         }
 
-        passwordLabel.snp.makeConstraints { make in
+        passwordView.snp.makeConstraints { make in
             make.top.equalTo(titleLabel.snp.bottom).offset(offset*2)
             make.horizontalEdges.equalToSuperview().inset(inset)
         }
 
-        passwordTextField.snp.makeConstraints { make in
-            make.top.equalTo(passwordLabel.snp.bottom).offset(offset/2)
-            make.horizontalEdges.equalToSuperview().inset(inset)
-            make.height.equalTo(height)
-        }
-
-        sameStackView.snp.makeConstraints { make in
-            make.top.equalTo(passwordTextField.snp.bottom).offset(offset*2)
+        reconfirmView.snp.makeConstraints { make in
+            make.top.equalTo(passwordView.snp.bottom).offset(offset)
             make.horizontalEdges.equalToSuperview().inset(inset)
         }
 
         nextButton.snp.makeConstraints { make in
-            make.top.equalTo(sameStackView.snp.bottom).offset(offset*2)
+            make.top.equalTo(reconfirmView.snp.bottom).offset(offset*2)
+            make.horizontalEdges.equalToSuperview().inset(inset)
+            make.height.equalTo(height)
+        }
+
+        prevButton.snp.makeConstraints { make in
+            make.top.equalTo(nextButton.snp.bottom).offset(offset)
             make.horizontalEdges.equalToSuperview().inset(inset)
             make.height.equalTo(height)
         }
