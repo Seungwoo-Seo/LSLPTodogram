215315b562d7236e2f38b6187d6f354a40fd0338
SeungwooSeo <prodvssw.98@gmail.com>
2023-11-30 23:41:16 +0900

    TodoNewsfeed Scene UI 테스트

--- LSLPTodogram/Scene/TodoNewsfeed/View/TodoNewsfeedMainView.swift
+++ LSLPTodogram/Scene/TodoNewsfeed/View/TodoNewsfeedMainView.swift
@@ -6,7 +6,97 @@
 //
 
 import UIKit
+import Kingfisher
 
 final class TodoNewsfeedMainView: BaseView {
+    var dataSource: UICollectionViewDiffableDataSource<TodoInfo, Todo>!
+
+    lazy var collectionView = {
+        let view = UICollectionView(
+            frame: .zero,
+            collectionViewLayout: createLayout()
+        )
+        view.backgroundColor = Color.lightGray
+        return view
+    }()
+
+    override func initialAttributes() {
+        super.initialAttributes()
+
+//        configureDataSource()
+    }
+
+    override func initialHierarchy() {
+        super.initialHierarchy()
+
+        addSubview(collectionView)
+    }
+
+    override func initialLayout() {
+        super.initialLayout()
+
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalTo(safeAreaLayoutGuide)
+        }
+    }
+
+}
+
+private extension TodoNewsfeedMainView {
+
+    func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { [weak self] (_, _) in
+            guard let self else {return nil}
+            return self.todoLayout()
+        }
+
+        return layout
+    }
+
+    func todoLayout() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .estimated(50)
+        )
+
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .estimated(50)
+        )
+
+        let group = NSCollectionLayoutGroup.vertical(
+            layoutSize: groupSize,
+            subitems: [item]
+        )
+
+        let headerSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .estimated(80)
+        )
+
+        let header = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: headerSize,
+            elementKind: TodoNewsfeedHeader.identifier,
+            alignment: .top
+        )
+
+        let footerSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .estimated(80)
+        )
+
+        let footer = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: footerSize,
+            elementKind: TodoNewsfeedFooter.identifier,
+            alignment: .bottom
+        )
+
+        let section = NSCollectionLayoutSection(group: group)
+        section.boundarySupplementaryItems = [header, footer]
+
+        return section
+    }
 
 }
--- LSLPTodogram/Scene/TodoNewsfeed/View/TodoNewsfeedViewController.swift
+++ LSLPTodogram/Scene/TodoNewsfeed/View/TodoNewsfeedViewController.swift
@@ -6,15 +6,73 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 final class TodoNewsfeedViewController: BaseViewController {
     private let mainView = TodoNewsfeedMainView()
+    private let disposeBag = DisposeBag()
 
     init(_ viewModel: TodoNewsfeedViewModel) {
         super.init(nibName: nil, bundle: nil)
 
+        let cellRegistration = UICollectionView.CellRegistration<TodoNewsfeedCell, Todo> { cell, indexPath, itemIdentifier in
+            cell.todoLabel.text = "\(itemIdentifier.title)"
+        }
+
+        let headerRegistration = UICollectionView.SupplementaryRegistration<TodoNewsfeedHeader>(
+            elementKind: TodoNewsfeedHeader.identifier
+        ) { (supplementaryView, elementKind, indexPath) in
+            let todoInfo = viewModel.todoInfoList.value[indexPath.item]
+            supplementaryView.profileImageView.image = todoInfo.profileImage
+            supplementaryView.nicknameLabel.text = todoInfo.nickname
+            supplementaryView.titleLabel.text = todoInfo.title
+        }
+
+        let footerRegistration = UICollectionView.SupplementaryRegistration<TodoNewsfeedFooter>(
+            elementKind: TodoNewsfeedFooter.identifier
+        ) { (supplementaryView, elementKind, indexPath) in
+            let todoInfo = viewModel.todoInfoList.value[indexPath.item]
+//            let todoInfo = viewModel.todoInfoList.value[indexPath.item]
+        }
+
+        mainView.dataSource = UICollectionViewDiffableDataSource(
+            collectionView: mainView.collectionView
+        ) { collectionView, indexPath, itemIdentifier in
+            return collectionView.dequeueConfiguredReusableCell(
+                using: cellRegistration,
+                for: indexPath,
+                item: itemIdentifier
+            )
+        }
+
+        mainView.dataSource.supplementaryViewProvider = { (collectionView, kind, indexPath) in
+            if kind == TodoNewsfeedHeader.identifier {
+                return collectionView.dequeueConfiguredReusableSupplementary(
+                    using: headerRegistration,
+                    for: indexPath
+                )
+            } else {
+                return collectionView.dequeueConfiguredReusableSupplementary(
+                    using: footerRegistration,
+                    for: indexPath
+                )
+            }
+        }
+
         let input = TodoNewsfeedViewModel.Input()
         let output = viewModel.transform(input: input)
+
+        output.todoInfoList
+            .bind(with: self) { owner, todoInfoList in
+                var snapshot = NSDiffableDataSourceSnapshot<TodoInfo, Todo>()
+                snapshot.appendSections(todoInfoList)
+                for todoInfo in todoInfoList {
+                    snapshot.appendItems(todoInfo.todoList, toSection: todoInfo)
+                }
+                owner.mainView.dataSource.apply(snapshot)
+            }
+            .disposed(by: disposeBag)
     }
 
     override func loadView() {
