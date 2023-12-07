1ef1549c734fa2922b3fab578e7dfac7ebf5590e
SeungwooSeo <prodvssw.98@gmail.com>
2023-12-07 16:25:21 +0900

    rootViewController 변경

--- LSLPTodogram/SceneDelegate.swift
+++ LSLPTodogram/SceneDelegate.swift
@@ -10,13 +10,20 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    private let loginViewModel = LoginViewModel()
+    private let todoNewsfeedViewModel = BupNewsfeedViewModel()
 
+    private let todoAddViewModel = TodoInputViewModel()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = LoginViewController()
+
+//        window?.rootViewController = TodoInputViewController(todoAddViewModel)
+
+        window?.rootViewController = UINavigationController(rootViewController: LoginViewController(loginViewModel))
+
         window?.makeKeyAndVisible()
     }
 
