2ebae2597867994519fb926238194e4c23d3abbe
SeungwooSeo <prodvssw.98@gmail.com>
2023-12-31 23:13:12 +0900

    Update SceneDelegate.swift

--- LSLPTodogram/SceneDelegate.swift
+++ LSLPTodogram/SceneDelegate.swift
@@ -16,11 +16,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         window = UIWindow(windowScene: windowScene)
 
-        if let _ = KeychainManager.read(key: KeychainKey.token.rawValue) {
-            window?.rootViewController = MainTabBarController()
-        } else {
-            window?.rootViewController = UINavigationController(rootViewController: LoginViewController())
-        }
+//        if let _ = KeychainManager.read(key: KeychainKey.token.rawValue) {
+//            window?.rootViewController = MainTabBarController()
+//        } else {
+//            window?.rootViewController = UINavigationController(rootViewController: LoginViewController())
+//        }
+
+        window?.rootViewController = UINavigationController(rootViewController: LoginViewController())
 
         window?.makeKeyAndVisible()
     }
